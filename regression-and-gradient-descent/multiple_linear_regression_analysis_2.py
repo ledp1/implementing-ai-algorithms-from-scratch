929dde01a14bbb2fc390aa003fd57dc8f2540fc9
luis-eduardo-de-pombo-puerta <depombo2@gmail.com>
2025-05-18 16:51:53 -0500

    Add multiple linear regression analysis 2 with housing data example

---
+++ regression-and-gradient-descent/multiple_linear_regression_analysis_2.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+# given data
+housing_data = np.array([[1800, 3], [2400, 4], [1416, 2], [3000, 5]])
+prices = np.array([350000, 475000, 230000, 640000])
+
+# adding 1s to our matrix
+ones = np.ones(shape=(len(housing_data), 1))
+X = np.append(ones, housing_data, axis=1)
+
+# calculating coefficients
+coefficients = np.linalg.inv(X.T @ X) @ X.T @ prices
+
+# predicting prices
+predicted_prices = X @ coefficients
+
+# calculating residuals
+residuals = prices - predicted_prices
+
+# calculating total sum of squares
+sst = np.sum((prices - np.mean(prices)) ** 2)
+
+# calculating residual sum of squares
+ssr = np.sum(residuals ** 2)
+
+# calculating R^2
+r2 = 1 - (ssr/sst)
+
+print("Coefficients:", coefficients)
+print("Predicted prices:", predicted_prices)
+print("R^2:", r2)
